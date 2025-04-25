8e8470dbda4a65483f2a16bc6df23fc4053aadf0
sadq <mamsa0205@gmail.com>
2025-04-25 18:28:20 +0330

    feat(database): add media writer

---
+++ internal/infrastructure/database/writer.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+
+	"nos3/internal/domain/model"
+)
+
+type MediaWriter struct {
+	db *Database
+}
+
+func NewMediaWriter(db *Database) *MediaWriter {
+	return &MediaWriter{db: db}
+}
+
+func (u *MediaWriter) Upload(ctx context.Context, media *model.Media) error {
+	ctx, cancel := context.WithTimeout(ctx, u.db.QueryTimeout)
+	defer cancel()
+
+	coll := u.db.Client.Database(u.db.DBName).Collection(MediaCollection)
+
+	_, err := coll.InsertOne(ctx, media)
+
+	return err
+}
