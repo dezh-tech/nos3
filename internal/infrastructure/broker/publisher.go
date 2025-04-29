445b6e1158056f5f7c246ff141b886357e29b5be
sadq <mamsa0205@gmail.com>
2025-04-29 21:33:28 +0330

    feat: add message broker publisher service

---
+++ internal/infrastructure/broker/publisher.go
@@ -0,0 +1,43 @@
+package broker
+
+import (
+	"context"
+	"errors"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"time"
+)
+
+type Publisher struct {
+	rabbitMQ *Client
+	timeout  time.Duration
+}
+
+func NewPublisher(rabbitMQ *Client, cfg PublisherConfig) *Publisher {
+	return &Publisher{
+		rabbitMQ: rabbitMQ,
+		timeout:  time.Duration(cfg.Timeout) * time.Millisecond,
+	}
+}
+
+func (p *Publisher) Publish(ctx context.Context, message string) error {
+	if p.rabbitMQ.channel == nil {
+		return errors.New("channel is not initialized")
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, p.timeout)
+	defer cancel()
+
+	return p.rabbitMQ.channel.PublishWithContext(
+		ctx,
+		"",
+		p.rabbitMQ.queueName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:  "text/plain",
+			Body:         []byte(message),
+			Timestamp:    time.Now(),
+			DeliveryMode: amqp.Persistent,
+		},
+	)
+}
