79098f5c71475046c329c80c67b8dfab8a643baf
sadq <mamsa0205@gmail.com>
2025-05-09 20:03:21 +0330

    feat: add redis publisher logic

---
+++ internal/infrastructure/broker/publisher.go
@@ -0,0 +1,34 @@
+package broker
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Publisher struct {
+	redis  *redis.Client
+	stream string
+	timout time.Duration
+}
+
+func NewPublisher(client *Client, cfg PublisherConfig) *Publisher {
+	return &Publisher{
+		redis:  client.redis,
+		stream: client.stream,
+		timout: time.Duration(cfg.Timeout) * time.Millisecond,
+	}
+}
+
+func (p *Publisher) Publish(ctx context.Context, message string) error {
+	if p.redis == nil {
+		return errors.New("redis not initialized")
+	}
+
+	return p.redis.XAdd(ctx, &redis.XAddArgs{
+		Stream: p.stream,
+		Values: map[string]interface{}{"body": message},
+	}).Err()
+}
